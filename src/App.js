1a80d30d3a99b8e59e7677de86a2cfa13be105af
HuguesRomain <huguesromainpro@gmail.com>
2021-03-03 18:57:06 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import styled from 'styled-components'
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
-function App() {
+
+
+const WrapperPageContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    /* justify-content: space-between; */
+    /* min-height: 100vh; */
+`
+
+const HeaderContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: grey;
+`;
+
+const MapAndList = styled.div`
+  display: flex;
+  margin-top: 15px;
+`;
+
+const List = styled.div`
+  background-color: grey;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: auto;
+  width: 30%;
+`; 
+
+const LeafLetContainer = styled(MapContainer)`
+  z-index: 0;
+  overflow: hidden;
+  border: 1px solid grey;
+  width: 70%;
+  height: calc(100vh - 300px);
+`
+
+const Red = styled.div`
+  background-color: red;
+  height: 120px;
+  width: 1000px;
+  margin: 10px 0;
+`
+
+
+
+const MapComponent = () => {
+  return (
+    <LeafLetContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </LeafLetContainer>
+  )
+}
+
+const App = () =>  {
+  const [plan, setPlan] = useState(["plan", 'plan'])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WrapperPageContent>
+        <HeaderContent>
+          {plan.map(() => {
+            return (
+              <Red onClick={() => {
+                  setPlan(oldArray => [...oldArray, 'plan']);
+                }}/>
+            )
+          })}
+        </HeaderContent>
+        <MapAndList>
+          <MapComponent />
+          <List />
+        </MapAndList>
+      </WrapperPageContent>
     </div>
   );
 }
