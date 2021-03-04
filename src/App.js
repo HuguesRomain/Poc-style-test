5e72dff430563e35d89a17bc7dc99ebc5bc5c336
HuguesRomain <huguesromainpro@gmail.com>
2021-03-04 14:46:06 +0100

    cards

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import styled from 'styled-components'
 import { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 
 
@@ -18,7 +18,7 @@ const HeaderContent = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: grey;
+  background-color: white;
 `;
 
 const MapAndList = styled.div`
@@ -27,49 +27,53 @@ const MapAndList = styled.div`
 `;
 
 const List = styled.div`
-  background-color: grey;
+  background-color: white;
   display: flex;
   flex-direction: column;
   align-items: center;
   overflow-y: auto;
-  width: 30%;
+  flex-grow: 1;
+  scroll-behavior: smooth;
+  width: 450px;
+  height: calc(100vh - 300px);
 `; 
 
-const LeafLetContainer = styled(MapContainer)`
+const Cards = styled.div`
+  min-height: 90px;
+  width: 80%;
+  background-color: blue;
+  margin: 10px 0;
+`;
+
+// const LeafLetContainer = styled(MapContainer)`
+//   z-index: 0;
+//   overflow: hidden;
+//   border: 1px solid white;
+//   width: 70%;
+//   height: calc(100vh - 300px);
+// `
+
+const MapContainerStyled = styled.div`
   z-index: 0;
   overflow: hidden;
-  border: 1px solid grey;
+  border: 1px solid white;
   width: 70%;
   height: calc(100vh - 300px);
-`
+  background-color: green;
+`;
 
 const Red = styled.div`
   background-color: red;
   height: 120px;
-  width: 1000px;
+  width: 80%;
   margin: 10px 0;
 `
 
 
 
-const MapComponent = () => {
-  return (
-    <LeafLetContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </LeafLetContainer>
-  )
-}
-
 const App = () =>  {
   const [plan, setPlan] = useState(["plan", 'plan'])
+  const [content] = useState(['content','content','content','content','content','content','content','content','content','content','content','content','content','content','content','content','content','content','content','content', ])
 
 
   return (
@@ -85,8 +89,14 @@ const App = () =>  {
           })}
         </HeaderContent>
         <MapAndList>
-          <MapComponent />
-          <List />
+          <MapContainerStyled />
+          <List>
+            {content.map(() => {
+              return(
+                <Cards />
+              )
+            })}
+          </List>
         </MapAndList>
       </WrapperPageContent>
     </div>
@@ -94,3 +104,32 @@ const App = () =>  {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const MapComponent = () => {
+//   return (
+//     <LeafLetContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+//       <TileLayer
+//         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       />
+//       <Marker position={[51.505, -0.09]}>
+//         <Popup>
+//           A pretty CSS3 popup. <br /> Easily customizable.
+//         </Popup>
+//       </Marker>
+//     </LeafLetContainer>
+//   )
+// }
+
